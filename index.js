6e2d1cee91f6461b77c054f1969d06b8d9d0c9ae
firulete <sw4ttt@gmail.com>
2017-01-26 14:00:27 -0400

    stuff

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ var app = express()
 var Stringjs = require('string');
 var http = require('http');
 var odbcwrapperphp = "./sw4ttt_modules/OdbcWrapperPhp/odbcwrapperphp.php"
+app.use(express.static(__dirname + '/public'));
 
 // INFORMACION BASICA PARA CONFIGURAR EL CLIENTE DEL SISTEMA LOCAL.
 var nombreTienda = "";
 var codigoSeg = "JtmzAMVx";
-var directorioBD = "D:/Web/a2testbd/DATA";
+var directorioBD = "C:/a2JEANSWEST/Empre001/DATA/";
 var infoBusqueda = "userdata";
 // ----------------------------------------------------------------.
 
 var processrunner = require('./sw4ttt_modules/processrunner');
 processrunner.callServer("http://shopinfopanel.herokuapp.com/api",codigoSeg,odbcwrapperphp,directorioBD+","+infoBusqueda);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res)
+{
+  //res.sendfile('index.html', { root: __dirname + '/views/pages' });
+  res.sendFile('config.html', {root: __dirname+ '/views/pages'});
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
