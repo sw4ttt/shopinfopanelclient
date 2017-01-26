c5e99b639e9407542d248865d041f014f9972e36
sw4ttt <sw4ttt@gmail.com>
2017-01-25 23:41:27 -0400

    full con cron

--- sw4ttt_modules/processrunner/index.js
+++ sw4ttt_modules/processrunner/index.js
@@ -1,25 +1,33 @@
 var httpshop = require('../httpshop');
 var runner = require("child_process");
+var cron = require('node-schedule');
 
 module.exports = {
   callServer: function (serverURL,codigoSeg,rutaScript,paramScript) 
   {
-      runner.exec("php " + rutaScript + " " +paramScript, function(err, dataSQL, stderr) 
-      {
-          if(stderr) 
-          {
-              console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
-              console.log("--ERROR: PHP-StdErr: \n"+stderr);
-              console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
-          }
-          else
-            if(dataSQL)
+        var j = cron.scheduleJob('*/1 * * * *', function()
+        {
+            console.log('CRON: START');  
+            //----------------------------------------------------------------------------------------------
+            runner.exec("php " + rutaScript + " " +paramScript, function(err, dataSQL, stderr) 
             {
-                console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
-                console.log("--SUCCESS: DATA: \n"+dataSQL);
-                console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
-                httpshop.getRequest(serverURL,codigoSeg,dataSQL);
-            }
-      });
+                if(stderr) 
+                {
+                    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    console.log("--ERROR: PHP-StdErr: \n"+stderr);
+                    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
+                }
+                else
+                    if(dataSQL)
+                    {
+                        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
+                        console.log("--SUCCESS: DATA: \n"+dataSQL);
+                        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>");
+                        httpshop.getRequest(serverURL,codigoSeg,dataSQL);
+                    }
+            });
+            //----------------------------------------------------------------------------------------------
+        });
+
   }
 };
