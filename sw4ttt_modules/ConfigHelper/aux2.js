cb0d09773a42a7b78f206e63f8a8717a4754f63c
sw4ttt <sw4ttt@gmail.com>
2017-01-27 11:03:32 -0400

    home last

--- sw4ttt_modules/ConfigHelper/aux2.js
+++
@@ -1,5 +0,0 @@
-var confighelper = require('./index.js');
-var httpshop = require('../httpshop');
-
-//console.log(confighelper.checkConfigData("http://shopinfopanel.herokuapp.com/api","JtmzAMVx"));
-console.log("httpshop.checkCodigoTienda = "+httpshop.checkCodigoTienda("http://shopinfopanel.herokuapp.com/api","JtmzAMVx"));
