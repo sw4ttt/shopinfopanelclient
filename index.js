8ea69d2b081d09e3e349e278de434add9ea52ce9
Oscar Marquez <sw4ttt@gmail.com>
2017-11-25 11:35:56 -0400

    Cambios en consulta de

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var app = express();
 var _ = require('lodash');
 var S = require('string');
 var http = require('http').createServer(app);
-var odbcwrapperphp = "./sw4ttt_modules/OdbcWrapperPhp/odbcwrapperphp.php";
-// var urlServer = 'https://shopinfopanel.herokuapp.com/api/test';
+
 var config = require('./models/config/model');
 var cron = require('./models/utils/cron/model');
 var router = require('./api/router');
@@ -27,105 +26,111 @@ app.use(express.static(__dirname + '/public'));
 // var infoBusqueda = "userdata";
 // ----------------------------------------------------------------.
 
-const sql = require('mssql')
-const configSql = {
-  user: 'sa',
-  password: 'boner1991',
-  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-  database: 'ICGFRONT2015',
-
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-}
-
-sql.connect(configSql,function (err) {
-  if(err)console.log("ERROR SQL=",err);
-  else{
-    var query = "SELECT TOP 10 NUMSERIE,NUMFACTURA,CODCLIENTE,FECHA,HORA,TOTALBRUTO,TOTALIMPUESTOS,TOTALNETO,TOTALCOSTE,TIPODOC FROM ICGFRONT2015.dbo.FACTURASVENTA ORDER BY FECHA DESC";
-    new sql.Request().query(query,function (err, result) {
-
-
-
-      console.log(result)
-    })
-
-// Stored Procedure
-
-    // new sql.Request()
-    //   .input('input_parameter', sql.Int, value)
-    //   .output('output_parameter', sql.VarChar(50))
-    //   .execute('procedure_name', function(err, result){
-    //     // ... error checks
-    //
-    //     console.dir(result)
-    //   })
-  }
-
-})
-
-sql.on('error', function(err){
-  // ... error handler
-})
-
-
-// config.checkConfig(function (err,configObject) {
-//     if (err)
-//     {
-//         console.log("SYSTEM-CONFIG-ERROR=",err)
-//     }
-//     else
-//     {
-//         app.use('/', router);
+// const sql = require('mssql')
+// const configSql = {
+//   user: 'sa',
+//   password: 'boner1991',
+//   server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+//   database: 'ICGFRONT2015',
 //
-//         io.on('connection', function(socket)
-//         {
-//             console.log('User connected');
-//             socket.on('disconnect', function ()
-//             {
-//                 console.log('User disconnected');
-//             });
-//         });
+//   options: {
+//     encrypt: true // Use this if you're on Windows Azure
+//   }
+// }
 //
-//         http.listen(3000, function(){
-//             console.log('listening on *:3000');
-//         });
+// sql.connect(configSql,function (err) {
+//   if(err)console.log("ERROR SQL=",err);
+//   else{
+//     var query = "SELECT TOP 10 NUMSERIE,NUMFACTURA,CODCLIENTE,FECHA,HORA,TOTALBRUTO,TOTALIMPUESTOS,TOTALNETO,TOTALCOSTE,TIPODOC FROM ICGFRONT2015.dbo.FACTURASVENTA ORDER BY FECHA DESC";
+//     new sql.Request().query(query,function (err, result) {
 //
-//         http.on('tlsClientError', function (e, socket) {
-//             console.log('tlsError');
-//             console.log(e);
-//         });
 //
-//         http.on('clientError', function (e) {
-//             console.log('clientError');
-//             console.log(e);
-//         });
 //
-//         http.on('resumeSession', function (id, cb) {
-//             console.log('resumeSess ' + id.toString('hex'));
-//         });
+//       console.log(result)
+//     })
 //
-//         var ioClient = require('socket.io-client')(configObject.remoteServer);
+// // Stored Procedure
 //
-//         ioClient.on('connect', function(){
-//             console.log('SOCKET-SERVER: connect')
+//     // new sql.Request()
+//     //   .input('input_parameter', sql.Int, value)
+//     //   .output('output_parameter', sql.VarChar(50))
+//     //   .execute('procedure_name', function(err, result){
+//     //     // ... error checks
+//     //
+//     //     console.dir(result)
+//     //   })
+//   }
 //
-//             ioClient.emit('call',{
-//                 type:"client",
-//                 msg:"me conecte desde tienda",
-//                 "idStore":configObject.idStore,
-//                 "nameStore":configObject.nameStore
-//             },function(data){
-//                 console.log("SOCKET-RESPONSE=",data)
-//             });
-//             ioClient.on('disconnect', function(){
-//                 console.log('SOCKET-SERVER: disconnect')
-//             });
-//         });
+// })
 //
-//         // cron.init(configObject.remoteServer);
-//         // cron.salesCron();
-//         // cron.clearLogs();
-//
-//     }
-// });
+// sql.on('error', function(err){
+//   // ... error handler
+// })
+
+
+config.checkConfig(function (err,configObject) {
+    if (err)
+    {
+        console.log("SYSTEM-CONFIG-ERROR=",err)
+    }
+    else
+    {
+        app.use('/', router);
+
+        io.on('connection', function(socket)
+        {
+            console.log('User connected');
+            socket.on('disconnect', function ()
+            {
+                console.log('User disconnected');
+            });
+        });
+
+        http.listen(3000, function(){
+            console.log('listening on *:3000');
+        });
+
+        http.on('tlsClientError', function (e, socket) {
+            console.log('tlsError');
+            console.log(e);
+        });
+
+        http.on('clientError', function (e) {
+            console.log('clientError');
+            console.log(e);
+        });
+
+        http.on('resumeSession', function (id, cb) {
+            console.log('resumeSess ' + id.toString('hex'));
+        });
+
+        var ioClient = require('socket.io-client')(configObject.remoteServer);
+
+        ioClient.on('connect', function(){
+            console.log('SOCKET-SERVER: connect')
+
+            ioClient.emit('call',{
+                type:"client",
+                msg:"me conecte desde tienda",
+                "idStore":configObject.idStore,
+                "nameStore":configObject.nameStore
+            },function(data){
+                console.log("SOCKET-RESPONSE=",data)
+            });
+            ioClient.on('disconnect', function(){
+                console.log('SOCKET-SERVER: disconnect')
+            });
+        });
+
+        var sales = require('./models/sales/model');
+
+        sales.getDocsIdToday(function (err,response) {
+            console.log("getDocsIdToday.err=",JSON.stringify(err))
+          console.log("getDocsIdToday.response=",JSON.stringify(response))
+        })
+        // cron.init(configObject.remoteServer);
+        // cron.salesCron();
+        // cron.clearLogs();
+
+    }
+});
