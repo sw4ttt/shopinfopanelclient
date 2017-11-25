7a90e664caecbe9a6ada94369059d22c8a79c5de
Oscar Marquez <oscar.marquez@xpectra.com>
2017-11-25 15:43:28 -0400

    sql

--- models/utils/sqlHelper/model.js
+++ models/utils/sqlHelper/model.js
@@ -1,32 +1,31 @@
 /**
  * Created by Oscar Marquez on 6/3/2017.
  */
-"use strict";
-var _ = require('lodash');
-var runner = require("child_process");
-var async = require('async');
-var S = require('string');
-var path = require('path');
-var normalize = require('normalize-path');
+'use strict'
+var _ = require('lodash')
+var runner = require('child_process')
+var async = require('async')
+var S = require('string')
+var path = require('path')
+var normalize = require('normalize-path')
 var configObject = require('./../../../config.json')
-var fs = require('fs');
+var fs = require('fs')
 var configFile = require('./../../../config.json')
-var sql = require('mssql');
+var sql = require('mssql')
 
-var model = {};
+var model = {}
 
-model.get = function (query,callback)
-{
-    var configSql = _.pick(configFile,['user','password','server','database']);
-    sql.connect(configSql,function (err) {
-        if(err)return callback(err);
-        else{
-            var query = "SELECT TOP 10 NUMFACTURA,CODCLIENTE,FECHA,HORA,TOTALBRUTO,TIPODOC FROM ICGFRONT2015.dbo.FACTURASVENTA ORDER BY FECHA DESC";
-            new sql.Request().query(query,function (err, result) {
-                return callback(null,result);
-            })
-        }
-    })
-};
+model.get = function (query, callback) {
+  var configSql = _.pick(configFile, ['user', 'password', 'server', 'database'])
+  sql.connect(configSql, function (err) {
+    if (err) return callback(err)
+    else {
+      // var query = "SELECT TOP 10 NUMFACTURA,CODCLIENTE,FECHA,HORA,TOTALBRUTO,TIPODOC FROM ICGFRONT2015.dbo.FACTURASVENTA ORDER BY FECHA DESC";
+      new sql.Request().query(query, function (err, result) {
+        return err ? callback(err) : callback(null, _.get(result, 'recordset', []))
+      })
+    }
+  })
+}
 
-module.exports = model;
+module.exports = model
